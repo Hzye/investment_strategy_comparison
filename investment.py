827c895b18240ea9ea604e35a20c2621f71b8dd8
Hzye <81663786+Hzye@users.noreply.github.com>
2026-01-16 12:53:37 +1100

    base ETFInvestment class

--- investment.py
+++ investment.py
@@ -17,3 +17,22 @@ class Investment(ABC):
 
     def log_status(self, year):
         self.history.append({"year": year, "value": self.value})
+
+class ETFInvestment(Investment):
+    def __init__(self, name, initial_value, annual_return_rate, mer_fee):
+        super().__init__(name, initial_value)
+        self.rate = annual_return_rate
+        self.mer_fee = mer_fee
+
+    def calculate_annual_return(self):
+        # standard compounds interest
+        growth = self.value * self.rate
+        self.value += growth
+        return growth
+
+    def apply_costs(self):
+        # management expense ratio (MER)
+        cost = self.value *self.mer_fee
+        self.value -= cost
+        return cost
+
